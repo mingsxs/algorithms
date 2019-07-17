372c575cca75ba3dcbb7509f84db8c6dd261aa12
mingsxs <mingli5@cisco.com>
2019-07-17 21:24:52 +0800

    add kmp pattern search.

---
+++ kmp-search/driver.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+extern int kmp_search(char *source, char *pattern);
+
+static void strip_space(char *str) 
+{
+    int i, shift = 0;
+
+    for(i=0; str[i]; i++) {
+        if(str[i]==' ' || str[i]=='\n') {
+            ++shift;
+        } else {
+            str[i-shift] = str[i];
+        }
+    }
+
+    str[i-shift] = '\0';
+}
+
+static void stdin_flush(void)
+{
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
+}
+
+int main(int argc, char **argv)
+{
+    int exit = 0;
+    char source[64];
+    char pattern[64];
+    char out[32], tmp;
+    int pos;
+
+    while(!exit) {
+        fprintf(stdout, "source: ");
+        fgets(source, 63, stdin);
+        fprintf(stdout, "pattern: ");
+        fgets(pattern, 63, stdin);
+        putchar('\n');
+
+        strip_space(source);
+        strip_space(pattern);
+        fprintf(stdout, "source: %s\n", source);
+        fprintf(stdout, "pattern: %s\n", pattern);
+
+        pos = kmp_search(source, pattern);
+        if (pos < 0) strcpy(out, "\nNot matched!\n\n");
+        else sprintf(out, "\nMatched at %d!\n\n", pos);
+        fprintf(stdout, "%s", out);
+
+        fprintf(stdout, "Continue?(Y/N): ");
+        tmp = getchar(); // offer choice to continue;
+        putchar('\n');
+        if (tmp != 'Y' && tmp != 'y') {
+            exit = 1;
+        }
+        stdin_flush();
+    }
+    return 0;
+}
+
+
---
+++ kmp-search/kmpSearch.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* external API interface */
+extern int kmp_search(const char *source, const char *pattern);
+
+/* local forward declarations */
+static int *kmp_partial_match_table_p = NULL;          /* kmp partial match index table */
+
+/* function to create kmp table */
+static void kmp_create_table(const char *pattern)
+{
+    int p_len = (int )strlen(pattern);
+    int i, j;
+
+    if(kmp_partial_match_table_p) {
+        free(kmp_partial_match_table_p);
+        kmp_partial_match_table_p = NULL;
+    }
+
+    kmp_partial_match_table_p = (int *)malloc(p_len*sizeof(int));
+    /* initialize with 0xFF to avoid 0 index case */
+    memset(kmp_partial_match_table_p, 0xff, p_len*sizeof(int));
+
+    kmp_partial_match_table_p[0] = 0;
+    /*
+     * hopefully, it's very easy to understand.
+     * j points to first part of the given pattern string which has all unique characters.
+     * i is traverse index of both the given pattern string and the kmp index table items.
+     * eg:
+     *    if pattern is 'ABCDABE', j will always traverse within first part with all unique
+     *    characters 'ABCD', and i is the traverse index of whole pattern string and local
+     *    kmp index table.
+     *
+     */
+
+    for(j=0,i=1; pattern[i]; ) {
+        if(pattern[i] == pattern[j]) {
+            kmp_partial_match_table_p[i++] = ++j;
+        } else {
+            if(j == 0) {
+                kmp_partial_match_table_p[i++] = j;
+            } else {
+                j = 0;
+            }
+        }
+    }
+
+}
+
+
+int kmp_search(const char *source, const char *pattern)
+{
+    int p_len = (int) strlen(pattern);
+    int i, j, shift;
+
+    kmp_create_table(pattern);
+
+    for(i=0,j=0; source[i]&&pattern[j]; ){
+        if(source[i]==pattern[j]) {
+            ++i;
+            ++j;
+            continue;
+        }
+        if(j == 0) {
+            ++i;
+            continue;
+        }
+
+        shift = j - kmp_partial_match_table_p[j-1];
+        j-=shift;
+    }
+
+    return !pattern[j] ? (i- p_len) : -1;
+}
